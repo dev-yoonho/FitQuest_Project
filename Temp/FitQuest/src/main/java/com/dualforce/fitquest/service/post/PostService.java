fc740cc3f7ad8297a3575b4aade47025b6a51bc0
Yoonho Cho <josephcyh@yonsei.ac.kr>
2024-11-22 14:31:55 +0900

    wip(service): 서비스 계층 구현 중 작업 저장
    서비스 계층 구현 작업을 진행 중이며, 현재 상태를 저장합니다.

--- Temp/FitQuest/src/main/java/com/dualforce/fitquest/service/post/PostService.java
+++ Temp/FitQuest/src/main/java/com/dualforce/fitquest/service/post/PostService.java
@@ -1,11 +1,24 @@
 package com.dualforce.fitquest.service.post;
 
+import com.dualforce.fitquest.model.dto.PostDto;
+
+import java.util.List;
+
 public interface PostService {
     // 게시글 등록
-    
+    int createPost(PostDto post);
+
     // 게시글 목록 표시
+    List<PostDto> readAllPosts();
+
     // 게시글 수정
+    PostDto editPost(PostDto post);
+
     // 게시글 삭제
+    int deletePost(int postid);
+
     // 게시글 조건 검색
+
+
     // 게시글 상세 페이지
 }
